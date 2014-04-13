232a0718f51b6827afe0d00ee09d794caf1cc604
Chris Hoffman <choffman@optoro.com>
2014-04-13 10:20:01 -0400

    Login kind of works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :record_user_email
 
+  private
+
+  def after_sign_in_path_for(resource)
+    "/#/users/#{User.find_by_email(params[:user][:email]).id}/home"
+  end
+
+  def record_user_email
+    @email = params[:email]
+  end
 end
