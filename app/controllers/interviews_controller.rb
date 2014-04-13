931858cf426d9628c4f6366d63f966c6e88028a2
Helio <helio.cola@gmail.com>
2014-04-13 05:31:23 -0400

    forms + some style

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -3,4 +3,20 @@ class InterviewsController < ApplicationController
   
   def index
   end
+  
+  def show
+    interview = Interview.find(params[:id])
+    
+    # template = "even" if params[:id].to_i.even?
+    # template = "odd" if params[:id].to_i.odd?
+    template = interview.respondant.first_name.downcase
+    
+    render :partial => template
+  end
+  
+  def edit
+    @interview = Interview.find(params[:id])
+    
+    render :partial => "edit"
+  end
 end
