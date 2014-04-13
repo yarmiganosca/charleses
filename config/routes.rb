f8352e30bc590a6af8d0bfd2ad22d8edd76b9d1f
Helio <helio.cola@gmail.com>
2014-04-13 08:44:53 -0400

    removed advocare route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => "home#advocare"
-  get 'advocare' => 'home#advocare'
+  # get 'advocare' => 'home#advocare'
 
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
