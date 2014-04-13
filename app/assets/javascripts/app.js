79012a45ddbb06d674406b4f1e024376f0c81992
Leigh Boone <lboone@5amsolutions.com>
2014-04-13 02:27:12 -0400

    chris' changes

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var app = angular.module('charleses',
+	['charleses.services',
+	'charleses.directives',
+	'charleses.controllers',
+	'mgcrea.ngStrap']);
+
+angular.module('charleses.services', []);
+angular.module('charleses.directives', []);
+angular.module('charleses.controllers', []);
+
---
+++ app/views/wireframes/js/directives.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* Directives */
+
+
+angular.module('charleses.directives', []).
+  directive('appVersion', ['version', function(version) {
+    return function() {
+      
+    };
+  }]);
+
