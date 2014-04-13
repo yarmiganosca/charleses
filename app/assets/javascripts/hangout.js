1e16dd0bdb621a6ddc1edf5310bbe78637a79bfb
Helio <helio.cola@gmail.com>
2014-04-13 06:28:49 -0400

    Google hangout

---
+++ app/assets/javascripts/hangout.js
@@ -0,0 +1,13 @@
+var init = function() {
+    gapi.hangout.render('createhangout-div', { 'render': 'createhangout' });
+};
+
+if (window.addEventListener) {
+    window.addEventListener("load", init, false);
+}
+else if (window.attachEvent) {
+    window.attachEvent("onload", init);
+}
+else {
+    window.onload = init;
+}
